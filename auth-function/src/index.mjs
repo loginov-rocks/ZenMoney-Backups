e3c10d54496f559b646e025f0dcaba560ed49cfa
Danila Loginov <danila@loginov.rocks>
2024-03-07 21:23:55 -0800

    Auth Function: Store token response in DynamoDB

--- auth-function/src/index.mjs
+++ auth-function/src/index.mjs
@@ -1,12 +1,16 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { SSMClient } from '@aws-sdk/client-ssm';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 
 import {
-  ZENMONEY_API_BASE_URL, ZENMONEY_API_CONSUMER_KEY_PARAMETER_NAME, ZENMONEY_API_CONSUMER_SECRET_PARAMETER_NAME,
-  ZENMONEY_API_REDIRECT_URI,
+  USERS_TABLE_NAME, ZENMONEY_API_BASE_URL, ZENMONEY_API_CONSUMER_KEY_PARAMETER_NAME,
+  ZENMONEY_API_CONSUMER_SECRET_PARAMETER_NAME, ZENMONEY_API_REDIRECT_URI,
 } from './Constants.mjs';
 import { SsmParameter } from './SsmParameter.mjs';
 import { ZenMoneyApi } from './ZenMoneyApi.mjs';
 
+const dynamoDbClient = new DynamoDBClient();
+const dynamoDbDocumentClient = DynamoDBDocumentClient.from(dynamoDbClient);
 const ssmClient = new SSMClient();
 
 const zenMoneyApiConsumerKeyParameter = new SsmParameter({
@@ -27,23 +31,69 @@ const zenMoneyApi = new ZenMoneyApi({
 export const handler = async (event) => {
   console.log('event', JSON.stringify(event));
 
-  const { body } = event;
-  const data = JSON.parse(body);
+  const { body, requestContext } = event;
+
+  if (!requestContext || !requestContext.authorizer || !requestContext.authorizer.jwt
+    || !requestContext.authorizer.jwt.claims || !requestContext.authorizer.jwt.claims.sub) {
+    return { statusCode: 401 };
+  }
+
+  const userId = requestContext.authorizer.jwt.claims.sub;
+
+  let data;
+  try {
+    data = JSON.parse(body);
+  } catch (error) {
+    console.error(error);
+
+    return { statusCode: 400 };
+  }
+
   const { zenMoneyAuthCode } = data;
 
-  const zenMoneyApiConsumerKey = await zenMoneyApiConsumerKeyParameter.getValue();
-  const zenMoneyApiConsumerSecret = await zenMoneyApiConsumerSecretParameter.getValue();
+  if (!zenMoneyAuthCode) {
+    return { statusCode: 400 };
+  }
+
+  let zenMoneyApiConsumerKey, zenMoneyApiConsumerSecret;
+  try {
+    zenMoneyApiConsumerKey = await zenMoneyApiConsumerKeyParameter.getValue();
+    zenMoneyApiConsumerSecret = await zenMoneyApiConsumerSecretParameter.getValue();
+  } catch (error) {
+    console.error(error);
+
+    return { statusCode: 500 };
+  }
 
   zenMoneyApi.setConsumerKey(zenMoneyApiConsumerKey);
   zenMoneyApi.setConsumerSecret(zenMoneyApiConsumerSecret);
 
-  const tokenResponse = await zenMoneyApi.token(zenMoneyAuthCode);
+  let token;
+  try {
+    token = await zenMoneyApi.token(zenMoneyAuthCode);
+  } catch (error) {
+    console.error(error);
 
-  return {
-    body: JSON.stringify(tokenResponse),
-    headers: {
-      'content-type': 'application/json',
-    },
-    statusCode: 200,
-  };
+    return { statusCode: 502 };
+  }
+
+  const item = { authorizedAt: Date.now(), userId, token };
+
+  const putCommand = new PutCommand({
+    Item: item,
+    TableName: USERS_TABLE_NAME,
+  });
+
+  let putCommandOutput;
+  try {
+    putCommandOutput = await dynamoDbDocumentClient.send(putCommand);
+  } catch (error) {
+    console.error(error);
+
+    return { statusCode: 500 };
+  }
+
+  console.log('putCommandOutput', JSON.stringify(putCommandOutput));
+
+  return { statusCode: 204 };
 };
