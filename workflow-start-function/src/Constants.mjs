4ad6212fdcb7638f01b6eedd0c3ad4e0c82fd789
Danila Loginov <danila@loginov.rocks>
2024-04-27 20:25:52 -0700

    Workflow Start Function: Keep execution ARN in DynamoDB to avoid concurrent executions

--- workflow-start-function/src/Constants.mjs
+++ workflow-start-function/src/Constants.mjs
@@ -1,1 +1,2 @@
+export const EXECUTIONS_TABLE_NAME = process.env.EXECUTIONS_TABLE_NAME;
 export const USERS_TABLE_NAME = process.env.USERS_TABLE_NAME;
--- workflow-start-function/src/index.mjs
+++ workflow-start-function/src/index.mjs
@@ -1,7 +1,7 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, GetCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
 
-import { USERS_TABLE_NAME } from './Constants.mjs';
+import { EXECUTIONS_TABLE_NAME, USERS_TABLE_NAME } from './Constants.mjs';
 
 const dynamoDbClient = new DynamoDBClient();
 const dynamoDbDocumentClient = DynamoDBDocumentClient.from(dynamoDbClient);
@@ -12,27 +12,62 @@ export const handler = async (event) => {
   const { current: { executionArn: currentExecutionArn }, previousExecutionArn, userId } = event;
 
   if (!currentExecutionArn || !userId) {
-    return { statusCode: 400 };
+    throw new Error('Current execution ARN or user ID missing');
   }
 
-  const getCommand = new GetCommand({
+  const usersGetCommand = new GetCommand({
     Key: { userId },
     TableName: USERS_TABLE_NAME,
   });
 
-  let getCommandOutput;
-  try {
-    getCommandOutput = await dynamoDbDocumentClient.send(getCommand);
-  } catch (error) {
-    console.error(error);
-
-    return { statusCode: 500 };
-  }
+  const usersGetCommandOutput = await dynamoDbDocumentClient.send(usersGetCommand);
 
   // Hide to avoid logging ZenMoney tokens.
-  // console.log('getCommandOutput', JSON.stringify(getCommandOutput));
+  // console.log('usersGetCommandOutput', JSON.stringify(usersGetCommandOutput));
 
-  const isAuthorized = Boolean(getCommandOutput && getCommandOutput.Item);
+  const isAuthorized = Boolean(usersGetCommandOutput && usersGetCommandOutput.Item);
 
-  return { isAuthorized, userId };
+  // No user in DynamoDB, return unauthorized.
+  if (!isAuthorized) {
+    return { isAuthorized, userId };
+  }
+
+  const executionsGetCommand = new GetCommand({
+    Key: { userId },
+    TableName: EXECUTIONS_TABLE_NAME,
+  });
+
+  const executionsGetCommandOutput = await dynamoDbDocumentClient.send(executionsGetCommand);
+
+  console.log('executionsGetCommandOutput', JSON.stringify(executionsGetCommandOutput));
+
+  // There is execution running for this user ID.
+  if (executionsGetCommandOutput && executionsGetCommandOutput.Item) {
+    const { executionArn } = executionsGetCommandOutput.Item;
+
+    // This is a new execution from ZenMoney Auth Function (no previous execution ARN) or a mismatch with the previous
+    // execution ARN.
+    if (!previousExecutionArn || previousExecutionArn !== executionArn) {
+      return { isAuthorized, isConcurrent: true, userId };
+    }
+  }
+
+  // If no execution is running for this user ID, or the previous execution ARN provided and matches the one in
+  // DynamoDB.
+  const item = {
+    executionArn: currentExecutionArn,
+    updated: Date.now(),
+    userId,
+  };
+
+  const executionsPutCommand = new PutCommand({
+    Item: item,
+    TableName: EXECUTIONS_TABLE_NAME,
+  });
+
+  const executionsPutCommandOutput = await dynamoDbDocumentClient.send(executionsPutCommand);
+
+  console.log('executionsPutCommandOutput', JSON.stringify(executionsPutCommandOutput));
+
+  return { isAuthorized, isConcurrent: false, userId };
 };
